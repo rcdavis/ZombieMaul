c4b7ae6453f9ff5bf8f28b5fbd8748cbef0a8637
Ren Davis <rcdavisgd@gmail.com>
2026-01-14 03:51:45 -0500

    Adjusted spacing for names in credits

--- Game/src/States/CreditsMenuState.cpp
+++ Game/src/States/CreditsMenuState.cpp
@@ -21,22 +21,22 @@ void CreditsMenuState::Enter() {
 	if (font) {
 		AddTextEntry("Programmers", font, sf::Vector2f(50.0f, 350.0f));
 		AddTextEntry("Ren Davis", font, sf::Vector2f(50.0f, 390.0f));
-		AddTextEntry("Kelsey Ishmael", font, sf::Vector2f(50.0f, 410.0f));
-		AddTextEntry("Wade Shwallon", font, sf::Vector2f(50.0f, 430.0f));
-		AddTextEntry("Andy Madruga", font, sf::Vector2f(50.0f, 450.0f));
-		AddTextEntry("Charlie Prouse", font, sf::Vector2f(50.0f, 470.0f));
-		AddTextEntry("Roger Prouse", font, sf::Vector2f(50.0f, 490.0f));
+		AddTextEntry("Kelsey Ishmael", font, sf::Vector2f(50.0f, 420.0f));
+		AddTextEntry("Wade Shwallon", font, sf::Vector2f(50.0f, 450.0f));
+		AddTextEntry("Andy Madruga", font, sf::Vector2f(50.0f, 480.0f));
+		AddTextEntry("Charlie Prouse", font, sf::Vector2f(50.0f, 510.0f));
+		AddTextEntry("Roger Prouse", font, sf::Vector2f(50.0f, 540.0f));
 
 		AddTextEntry("Artist", font, sf::Vector2f(300.0f, 350.0f));
 		AddTextEntry("Stacey Miller", font, sf::Vector2f(300.0f, 390.0f));
-		AddTextEntry("Jesse Williams", font, sf::Vector2f(300.0f, 410.0f));
+		AddTextEntry("Jesse Williams", font, sf::Vector2f(300.0f, 420.0f));
 
-		AddTextEntry("Audio Engineer", font, sf::Vector2f(300.0f, 450.0f));
-		AddTextEntry("Jesse Williams", font, sf::Vector2f(300.0f, 490.0f));
+		AddTextEntry("Audio Engineer", font, sf::Vector2f(300.0f, 480.0f));
+		AddTextEntry("Jesse Williams", font, sf::Vector2f(300.0f, 520.0f));
 
 		AddTextEntry("Special Thanks", font, sf::Vector2f(550.0f, 350.0f));
 		AddTextEntry("Zach Gurley", font, sf::Vector2f(550.0f, 390.0f));
-		AddTextEntry("Jeff Chilson", font, sf::Vector2f(550.0f, 410.0f));
+		AddTextEntry("Jeff Chilson", font, sf::Vector2f(550.0f, 420.0f));
 	}
 }
 
