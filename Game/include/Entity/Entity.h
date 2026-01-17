b7f384f1bcdecc5ac06cc2edf53a33a4a2b5e3b2
Ren Davis <rcdavisgd@gmail.com>
2026-01-17 09:58:10 -0500

    Added starting entity and player

---
+++ Game/include/Entity/Entity.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <optional>
+
+#include "SFML/Graphics/Sprite.hpp"
+
+namespace sf {
+	class RenderTarget;
+}
+
+class Game;
+
+class Entity {
+public:
+	enum class Type {
+		eEntity,
+		ePlayer,
+		ePerson,
+		eZombie,
+		eGuard
+	};
+
+public:
+	Entity(Game& game);
+	virtual ~Entity();
+
+	Entity(const Entity&) = default;
+	Entity& operator=(const Entity&) = default;
+
+	virtual void Update();
+	virtual void Render(sf::RenderTarget* const renderTarget);
+
+	void SetSprite(sf::Sprite sprite) { mSprite = sprite; }
+
+	void SetOrigin(sf::Vector2f pos) { mSprite->setOrigin(pos); }
+	const sf::Vector2f GetOrigin() const { return mSprite->getOrigin(); }
+
+	void Move(sf::Vector2f pos) { mSprite->move(pos); }
+	void SetPosition(sf::Vector2f pos) { mSprite->setPosition(pos); }
+	const sf::Vector2f GetPosition() const { return mSprite->getPosition(); }
+
+	void Rotate(sf::Angle rotation) { mSprite->rotate(rotation); }
+	void SetRotation(sf::Angle rotation) { mSprite->setRotation(rotation); }
+	const sf::Angle GetRotation() const { return mSprite->getRotation(); }
+
+	void SetSpeed(float speed) { mSpeed = speed; }
+	const float GetSpeed() const { return mSpeed; }
+
+	void SetType(Type type) { mType = type; }
+	Type GetType() const { return mType; }
+
+	void SetTextureRect(sf::IntRect rect) { mSprite->setTextureRect(rect); }
+
+	void SetTexture(sf::Texture* const texture) { mSprite->setTexture(*texture); }
+
+	const sf::Vector2f GetDirection() const;
+
+	sf::FloatRect getGlobalBounds() const { return mSprite->getGlobalBounds(); }
+
+private:
+	std::optional<sf::Sprite> mSprite;
+
+	Game& mGame;
+
+	float mSpeed;
+	Type mType;
+};
---
+++ Game/src/Entity/Player.cpp
@@ -0,0 +1,22 @@
+#include "Entity/Player.h"
+
+#include "Input/Input.h"
+#include "Game.h"
+
+Player::Player(Game& game) :
+	Entity(game)
+{
+	SetType(Type::ePerson);
+}
+
+Player::~Player() {
+
+}
+
+void Player::Update() {
+	if (Input::IsKeyDown(sf::Keyboard::Key::Left)) {
+		Rotate(sf::degrees(-4.0f));
+	} else if (Input::IsKeyDown(sf::Keyboard::Key::Right)) {
+		Rotate(sf::degrees(4.0f));
+	}
+}
