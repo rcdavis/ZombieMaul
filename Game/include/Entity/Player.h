79da52a61db047e9b99fa3975244dd94d8d2ed38
Ren Davis <rcdavisgd@gmail.com>
2026-01-31 21:02:30 -0500

    Add Player to Person collisions (#66)
    * Add converting to zombie
    
    * Added collision with zombie form

--- Game/include/Entity/Player.h
+++ Game/include/Entity/Player.h
@@ -2,10 +2,18 @@
 
 #include "Entity/Entity.h"
 
+class Person;
+
 class Player : public Entity {
 public:
 	Player(Game& game);
 	~Player();
 
 	void Update() override;
+
+	void HandleCollision(Entity* entity) override;
+
+private:
+	void HandlePersonCollision(Person* person);
+	void HandleZombieCollision(Person* zombie);
 };
--- Game/src/Entity/Player.cpp
+++ Game/src/Entity/Player.cpp
@@ -2,6 +2,9 @@
 
 #include "Input/Input.h"
 #include "Game.h"
+#include "Entity/Person.h"
+#include "Utils/Math.h"
+#include "Utils/Log.h"
 
 Player::Player(Game& game) :
 	Entity(game)
@@ -21,4 +24,51 @@ void Player::Update() {
 	}
 
 	Entity::Update();
+}
+
+void Player::HandleCollision(Entity* entity) {
+	switch (entity->GetType()) {
+	case Entity::Type::ePerson:
+		HandlePersonCollision((Person*)entity);
+		break;
+
+	case Entity::Type::eZombie:
+		HandleZombieCollision((Person*)entity);
+		break;
+	}
+}
+
+void Player::HandlePersonCollision(Person* person) {
+	const Circle personCircle {
+		.pos = person->GetPosition(),
+		.radius = 32.0f
+	};
+	const Circle playerCircle {
+		.pos = GetPosition(),
+		.radius = 32.0f
+	};
+
+	if (CircleCollision(playerCircle, personCircle)) {
+		person->ConvertToZombie();
+		// TODO: Send collision event
+	}
+}
+
+void Player::HandleZombieCollision(Person* zombie) {
+	if (!zombie->CanHurtPlayer())
+		return;
+
+	const Circle zombieCircle {
+		.pos = zombie->GetPosition(),
+		.radius = 32.0f
+	};
+	const Circle playerCircle {
+		.pos = GetPosition(),
+		.radius = 32.0f
+	};
+
+	if (CircleCollision(playerCircle, zombieCircle)) {
+		// TODO: Send collision event
+		LOG_INFO("Player collided with zombie");
+	}
 }
